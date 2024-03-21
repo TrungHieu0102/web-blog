b19d47bd769cd85b7f9d16737e301df75551346f
TrungHieu0102 <hatrunghieu121@gmail.com>
2024-03-21 15:54:43 +0700

    Logout and loading

--- web-blog-admin-ui/src/app/views/auth/login/login.component.ts
+++ web-blog-admin-ui/src/app/views/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , OnDestroy} from '@angular/core';
 import {
   FormBuilder,
   FormControl,
@@ -14,13 +14,16 @@ import {
 import { AlertService } from 'src/app/shared/services/alert.service';
 import { UrlConstants } from 'src/app/shared/constants/url.constants';
 import { TokenStorageService } from 'src/app/shared/services/token-storage.service';
+import { Subject, takeUntil } from 'rxjs';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnDestroy{
   loginForm: FormGroup;
+  private ngUnsubscribe = new Subject<void>();
+  loading = false;
   constructor(
     private fb: FormBuilder,
     private authApiClient: AdminApiAuthApiClient,
@@ -33,13 +36,21 @@ export class LoginComponent {
       password: new FormControl('', Validators.required),
     });
   }
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
   login() {
-    var request: LoginRequest = new LoginRequest({
+    this.loading = true;
+    var request:
+     LoginRequest = new LoginRequest({
       userName: this.loginForm.controls['userName'].value,
       password: this.loginForm.controls['password'].value,
     });
 
-    this.authApiClient.login(request).subscribe({
+    this.authApiClient.login(request)
+    .pipe(takeUntil(this.ngUnsubscribe))
+    .subscribe({
       next: (res: AuthenticatedResult) => {
         //Save token and refresh token to localstorage
         this.tokenSerivce.saveToken(res.token);
@@ -51,6 +62,7 @@ export class LoginComponent {
       error: (error: any) => {
         console.log(error);
         this.alertService.showError('Đăng nhập không đúng.');
+        this.loading = false;
       },
     });
   }

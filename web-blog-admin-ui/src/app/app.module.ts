956813c0388e5a3a20d98d0c05797f736cbe9042
TrungHieu0102 <hatrunghieu121@gmail.com>
2024-03-21 16:23:05 +0700

    add permissions

--- web-blog-admin-ui/src/app/app.module.ts
+++ web-blog-admin-ui/src/app/app.module.ts
@@ -44,7 +44,7 @@ import { MessageService } from 'primeng/api';
 import { AlertService } from './shared/services/alert.service';
 import { HttpClientModule } from '@angular/common/http';
 import { TokenStorageService } from './shared/services/token-storage.service';
-
+import { AuthGuard } from './shared/auth.guard';
 const APP_CONTAINERS = [
   DefaultFooterComponent,
   DefaultHeaderComponent,
@@ -97,6 +97,7 @@ const APP_CONTAINERS = [
     AlertService,
     AdminApiAuthApiClient,
     TokenStorageService,
+    AuthGuard,
   ],
   bootstrap: [AppComponent]
 })
--- web-blog-admin-ui/src/app/views/content/content-routing.module.ts
+++ web-blog-admin-ui/src/app/views/content/content-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostComponent } from './posts/post.component';
+import { AuthGuard } from 'src/app/shared/auth.guard';
+
 const routes: Routes = [
   {
     path: '',
@@ -11,8 +13,10 @@ const routes: Routes = [
     path: 'posts',
     component: PostComponent,
     data: {
-      title: 'Posts',
+      title: 'Bài Viết',
+      requiredPolicy: 'Permissions.Posts.View',
     },
+    canActivate: [AuthGuard],
   },
 ];
 
